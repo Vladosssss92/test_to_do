99a9e9479ebf94aac9aed7902d0c251ef7294ec6
Bespalyy Vladislav <Jawa0131@gmail.com>
2025-03-12 10:58:25 +0300

    priority sorting fix

--- src/constants/pryorityLevel.ts
+++ src/constants/pryorityLevel.ts
@@ -3,14 +3,19 @@ import { Priority } from "../types/taskTypes";
 export interface PriorityLevel {
     label: string,
     value: Priority
+    priority: number,
 }
 
-export type PriorityFilter = Priority | 'all' 
+export type PriorityFilter = Priority | 'all'
 
-export const prioritySorted = ['low', 'medium', 'hight']
+export const prioritySorted = {
+    'low': 0, 
+    'medium': 1, 
+    'high' : 2
+}
 
 export const PRIORITY_LEVELS: PriorityLevel[] = [
-    { label: "Низкий", value: "low" },
-    { label: "Средний", value: "medium" },
-    { label: "Высокий", value: "high" },
+    { label: "Низкий", value: "low", priority: 0 },
+    { label: "Средний", value: "medium", priority: 1 },
+    { label: "Высокий", value: "high", priority: 2 },
 ];
--- src/hooks/useSortedTasks.ts
+++ src/hooks/useSortedTasks.ts
@@ -7,8 +7,8 @@ export const useSortedTasks = (tasks: ITask[]) => {
     const [sorted, setSorted] = useState<SortedValue>('dateAscending');
 
     const formattedTaskArray = [...tasks].sort((a, b) => {
-        if (sorted === 'priorityAscending') return prioritySorted.indexOf(a.priority) - prioritySorted.indexOf(b.priority)
-        if (sorted === 'priorityDescending') return prioritySorted.indexOf(b.priority) - prioritySorted.indexOf(a.priority)
+        if (sorted === 'priorityAscending') return prioritySorted[a.priority] - prioritySorted[b.priority]
+        if (sorted === 'priorityDescending') return prioritySorted[b.priority] - prioritySorted[a.priority]
         if (sorted === 'dateAscending') return new Date(a.date).getTime() - new Date(b.date).getTime()
         if (sorted === 'dateDescending') return new Date(b.date).getTime() - new Date(a.date).getTime()
         return 0
